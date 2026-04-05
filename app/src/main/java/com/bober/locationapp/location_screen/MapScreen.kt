41427deee11062306fa1a047bf5d128cb7139c7f
Bober1337IT <bartekpietras03@gmail.com>
2026-04-05 23:15:08 +0200

    Enable location component in MapScreen and set portrait orientation for MainActivity

--- app/src/main/java/com/bober/locationapp/location_screen/MapScreen.kt
+++ app/src/main/java/com/bober/locationapp/location_screen/MapScreen.kt
@@ -1,12 +1,12 @@
 package com.bober.locationapp.location_screen
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.data.position
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
@@ -16,8 +16,11 @@ import org.maplibre.android.annotations.Marker
 import org.maplibre.android.annotations.MarkerOptions
 import org.maplibre.android.camera.CameraUpdateFactory
 import org.maplibre.android.geometry.LatLng
+import org.maplibre.android.location.LocationComponentActivationOptions
+import org.maplibre.android.location.modes.RenderMode
 import org.maplibre.android.maps.MapView
 
+@SuppressLint("MissingPermission")
 @Composable
 fun MapScreen(latitude: Double?, longitude: Double?) {
 
@@ -55,32 +58,40 @@ fun MapScreen(latitude: Double?, longitude: Double?) {
         modifier = Modifier.fillMaxSize()
     ) { mv ->
         mv.getMapAsync { map ->
-            map.setStyle("https://basemaps.cartocdn.com/gl/positron-gl-style/style.json")
+            map.setStyle("https://basemaps.cartocdn.com/gl/positron-gl-style/style.json") { style ->
 
-            var newPoint: Marker? = null
+                val locationComponent = map.locationComponent
 
-            if (latitude != null && longitude != null) {
-                map.moveCamera(
-                    CameraUpdateFactory.newLatLngZoom(
-                        latLng = LatLng(latitude, longitude),
-                        zoom = 13.0
+                val activationOptions = LocationComponentActivationOptions
+                    .builder(context, style)
+                    .useDefaultLocationEngine(true)
+                    .build()
+
+                locationComponent.activateLocationComponent(activationOptions)
+
+                locationComponent.isLocationComponentEnabled = true
+
+                locationComponent.renderMode = RenderMode.COMPASS
+
+                var newPoint: Marker? = null
+
+                if (latitude != null && longitude != null) {
+                    val userLatLng = LatLng(latitude, longitude)
+
+                    map.moveCamera(
+                        CameraUpdateFactory.newLatLngZoom(userLatLng, 13.0)
                     )
-                )
-                map.addMarker(
-                    MarkerOptions()
-                        .position(LatLng(latitude, longitude))
-                )
-                map.addOnMapClickListener { point ->
 
-                    if (newPoint != null) {
-                        newPoint?.position = point
-                    } else {
-                        newPoint = map.addMarker(
-                            MarkerOptions()
-                                .position(point)
-                        )
+                    map.addOnMapClickListener { point ->
+                        if (newPoint != null) {
+                            newPoint?.position = point
+                        } else {
+                            newPoint = map.addMarker(
+                                MarkerOptions().position(point)
+                            )
+                        }
+                        true
                     }
-                    true
                 }
             }
         }
